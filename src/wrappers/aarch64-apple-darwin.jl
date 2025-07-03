cef4226d190171c533b49c9eb17c3a2e0aace363
jlbuild <juliabuildbot@gmail.com>
2025-07-03 21:14:20 +0000

    libpng_jll build 1.6.50+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll)
     JLLWrappers.@init_library_product(
         libpng,
-        "lib/libpng16.16.49.0.dylib",
+        "lib/libpng16.16.50.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
