7cc355e1bdbe99c92a3517fc0a42af8a2a90b80b
jlbuild <juliabuildbot@gmail.com>
2025-12-07 17:34:09 +0000

    libpng_jll build 1.6.53+0

--- src/libpng_jll.jl
+++ src/libpng_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libpng")
-JLLWrappers.@generate_main_file("libpng", UUID("b53b4c65-9356-5827-b1ea-8c7a1a84506f"))
+JLLWrappers.@generate_main_file("libpng", Base.UUID("b53b4c65-9356-5827-b1ea-8c7a1a84506f"))
 end  # module libpng_jll
